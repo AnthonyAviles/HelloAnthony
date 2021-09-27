97c65f78c3740558ba0e7abf21acb2c68c218a93
AnthonyAviles <anthonyjaviles03@outlook.com>
2021-09-27 12:33:03 -0700

    Basic file

---
+++ src/com/java/tutorial/project/Main.java
@@ -0,0 +1,10 @@
+package com.java.tutorial.project;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String name = "anthony";
+        System.out.println(name);
+    }
+}
